9841a5d620f177ebc130a8858df639af2600f286
Kent Eudy <keudy@vizuri.com>
2018-11-28 20:22:44 -0500

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,48 +1,48 @@
 #!/usr/bin/groovy
 
 pipeline {
-	agent {
-		environment {
-			RELEASE_NUMBER = "";
-			nexusUrl = "http://nexus-student-1-cicd.apps.ocp-nonprod-01.kee.vizuri.com";
-		}
-		stages {
-			node ("maven") {
+	agent any
+	environment {
+		RELEASE_NUMBER = "";
+		nexusUrl = "http://nexus-student-1-cicd.apps.ocp-nonprod-01.kee.vizuri.com";
+	}
+	stages {
+		node ("maven") {
 
-				stage('Checkout') {
-					echo "In checkout"
-					checkout scm
+			stage('Checkout') {
+				echo "In checkout"
+				checkout scm
 
-					echo ">>>>>>  Branch Name: " + BRANCH_NAME;
-					def release_number;
+				echo ">>>>>>  Branch Name: " + BRANCH_NAME;
+				def release_number;
 
-					if(BRANCH_NAME.startsWith("release")) {
-						def tokens = BRANCH_NAME.tokenize( '/' )
-						branch_name = tokens[0]
-						branch_release_number = tokens[1]
+				if(BRANCH_NAME.startsWith("release")) {
+					def tokens = BRANCH_NAME.tokenize( '/' )
+					branch_name = tokens[0]
+					branch_release_number = tokens[1]
 
-						release_number = branch_release_number
-					}
-					else {
-						sh (
-								script: "mvn -B help:evaluate -Dexpression=project.version | grep -e '^[^\\[]' > release.txt",
-								returnStdout: true,
-								returnStatus: false
-								)
-						release_number = readFile('release.txt').trim()
-						echo "release_number: ${release_number}"
-					}
-
-					env.RELEASE_NUMBER = release_number;
+					release_number = branch_release_number
 				}
-			}
+				else {
+					sh (
+							script: "mvn -B help:evaluate -Dexpression=project.version | grep -e '^[^\\[]' > release.txt",
+							returnStdout: true,
+							returnStatus: false
+							)
+					release_number = readFile('release.txt').trim()
+					echo "release_number: ${release_number}"
+				}
 
-			stage('Build') {
-				echo "In Build"
-				sh "mvn -s configuration/settings.xml -Dnexus.url=${env.nexusUrl}  -DskipTests=true -Dbuild.number=${env.RELEASE_NUMBER} clean install"
+				env.RELEASE_NUMBER = release_number;
 			}
+		}
+
+		stage('Build') {
+			echo "In Build"
+			sh "mvn -s configuration/settings.xml -Dnexus.url=${env.nexusUrl}  -DskipTests=true -Dbuild.number=${env.RELEASE_NUMBER} clean install"
 		}
 	}
+	
 }
 
 
