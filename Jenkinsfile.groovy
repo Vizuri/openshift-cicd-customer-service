988637be907a231cfec27fb87538d8d420709a30
Kent Eudy <keudy@vizuri.com>
2018-11-29 17:28:47 -0500

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,12 @@ def app_name = "customer";
 def nexusUrl = "http://nexus-student-1-cicd.apps.ocp-nonprod-01.kee.vizuri.com";
 def release_number;
 
+def ocpCluster = "ocp-nonprod-01"
+def ocpDevProject = "student-1-customer-dev"
+def ocpTestProject = "student-1-customer-test"
+def ocpProdProject = "student-1-customer-prod"
+
+
 node ("maven-podman") {
 
 	stage('Checkout') {
@@ -65,19 +71,53 @@ node ("maven-podman") {
 	}
 	if (BRANCH_NAME ==~ /(develop|release.*)/) {
 		def tag = "${release_number}"
-		stage('Deploy Build Artifact') {
-			sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} -Dnexus.url=${nexusUrl} deploy"	
-		}
-		stage('Container Build') {
-			sh "podman build -t ${imageBase}/${imageNamespace}/${app_name}:${tag} ."
-		}
+		stage('Deploy Build Artifact') { sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} -Dnexus.url=${nexusUrl} deploy"	 }
+		stage('Container Build') { sh "podman build -t ${imageBase}/${imageNamespace}/${app_name}:${tag} ." }
 		stage('Container Push') {
 			sh "podman login -u ${registryUsername} -p ${registryPassword} ${imageBase}"
 			sh "podman push ${imageBase}/${imageNamespace}/${app_name}:${tag}"
 		}
 		stage('Container Scan') {
-			writeFile file: 'anchore_images', text: "${imageBase}/${imageNamespace}/${app_name}:${tag} Dockerfile"
-			anchore name: 'anchore_images'
+//			writeFile file: 'anchore_images', text: "${imageBase}/${imageNamespace}/${app_name}:${tag} Dockerfile"
+//			anchore name: 'anchore_images'
+		}
+	}
+	
+	if (BRANCH_NAME ==~ /(develop/) {
+		def ocp_project = ocp_dev_project;
+		
+		stage("Deploy Openshift ${ocp_project}") {
+			echo "In Deploy: ${ocp_cluster} : ${ocp_project} : ${app_name}"
+			openshift.withCluster( "${ocp_cluster}" ) {
+				openshift.withProject( "${ocp_project}" ) {
+					def dc = openshift.selector("dc", "${app_name}")
+					echo "DC: " + dc
+					echo "DC Exists: " + dc.exists()
+					if(!dc.exists()) {
+						echo "DC Does Not Exist Creating"
+						//dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=${Globals.imageBase}/${ocp_project}/${app_name}:latest -p APP_NAME=${app_name}").narrow("dc")
+						dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=${Globals.imageBase}/${Globals.imageNamespace}/${app_name}:${tag} -p APP_NAME=${app_name}").narrow("dc")
+					}
+					else {
+						def dcObject = dc.object()
+						dcObject.spec.template.spec.containers[0].image = "${Globals.imageBase}/${Globals.imageNamespace}/${app_name}:${tag}"
+						openshift.apply(dcObject)
+					}
+
+					def rm = dc.rollout()
+					rm.latest()
+					timeout(5) {
+						def latestDeploymentVersion = openshift.selector('dc',"${app_name}").object().status.latestVersion
+						echo "Got LatestDeploymentVersion:" + latestDeploymentVersion
+						def rc = openshift.selector('rc', "${app_name}-${latestDeploymentVersion}")
+						echo "Got RC" + rc
+						rc.untilEach(1){
+							def rcMap = it.object()
+							return (rcMap.status.replicas.equals(rcMap.status.readyReplicas))
+						}
+					}
+				}
+			}
 		}
 	}
 }
